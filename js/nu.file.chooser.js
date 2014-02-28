aff3845d6f4bd45ae3ecae1a7dcc97adb7845fc5
Kamalakar Gadireddy <kamal590@gmail.com>
2014-02-28 18:29:37 +0530

    Update File Chooser using ngModel insted for custom controler

--- js/nu.file.chooser.js
+++ js/nu.file.chooser.js
@@ -2,63 +2,24 @@
 
 var chooser = angular.module('nu.file.chooser', []);
 
-chooser.directive('nuFileChooser', ['$parse',
-  function($parse) {
+chooser.directive('nuFileChooser', [
+  function() {
+    'use strict';
     var _template =
     '<label class="nu file chooser" style="position: relative;">' +
-      '<input type="file"/>{{path}}' +
-      '<a ng-click="clear()" class="remove"></a>' +
+      '<input type="file"/><span></span>' +
+      '<a class="remove"></a>' +
     '</label>';
 
     return {
       template: _template,
       restrict: 'EACM',
       replace: true,
-      //require: '?ngModel',
-      scope: {},
-      controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-        this.$guess_type = null; //function(type, path) {}; // type mime type, path
-        var nuFile = this;
-        var update_src = function(put_src) {
-          return function(src) {
-            if( $scope.path !== src ) {
-              if(nuFile.$toSrc) {
-                src = nuFile.$toSrc(src);
-              }
-              put_src($scope.$parent, src);
-            }
-          };
-        };
-        this.$put_src = angular.noop;
-        this.$onSelection = angular.noop;
-        this.$onClear = angular.noop;
-        this.$toPath = function(src) {
-          return (src && src.name)? src.name : src;
-        };
-        this.$toSrc = null;
-
-        $attrs.$observe('src', function (value) {
-          // Can go to infinite loop need to check
-          nuFile.$put_src = angular.noop;
-          var ext = '';
-          var state = 'select';
-          if(value) {
-            var src = $parse(value);
-            $scope.path = basename(nuFile.$toPath(src($scope.$parent)));
-            if(src.assign) {
-              nuFile.$put_src = update_src(src.assign);
-            }
-            if ($scope.path && '' !== $scope.path.trim()) {
-              ext = splitext($scope.path).pop();
-              state = 'selected';
-            }
-          }
-          $element.attr('ext', ext);
-          $element.attr('state', state);
-        });
-      }],
+      require: '?ngModel',
       compile: function compile($element) { // $attrs
         var input = $element.find('input');
+        var name = $element.find('span');
+        var remove = $element.find('a');
 
         var update_attrs = function(ext, mime, state) {
           $element.attr('ext', ext);
@@ -66,39 +27,48 @@ chooser.directive('nuFileChooser', ['$parse',
           $element.attr('state', state);
         };
 
-        var link = function(scope, element, attrs, nuFile) {
-          var file_selected = function(event) {
-            var ext = '';
-            var mime = '';
-            var file = null;
-            var state = 'select';
+        var link = function(scope, element, attrs, ngModel) {
 
-            if(event.target.files.length > 0) {
-              file = event.target.files[0];
-              var splitPath = splitext(file.name);
-              ext = splitPath.length > 1? splitPath[1].toLowerCase() : splitPath[0];
-              mime = file.type;
-              if( nuFile.$guess_type ) {
-                mime = nuFile.$guess_type.call(file, mime, file.name, ext);
-              }
-              state = 'selected';
+          var fileFormatter = function(value) {
+            if(angular.isDefined(value)) {
+              var ext, mime, path;
+              
+              if(angular.isDefined(value.name)) {
+                mime = value.type;
+                path = value.name;
+              } else { path = value; }
+
+              var splitPath = splitext(path);
+              ext = (splitPath.length > 1? splitPath[1] : splitPath[0]).toLowerCase();
+              update_attrs(ext, mime, 'selected');
+              return basename(path);
             }
-            scope.$apply(function(scope) {
-              scope.path = file? file.name : '';
-              nuFile.$put_src(file);
+            return value;
+          };
+
+          ngModel.$formatters.push(fileFormatter);
+
+          ngModel.$render = function() {
+            name.html(ngModel.$viewValue);
+          };
+  
+          remove.on('click', function() {
+            name.html('');
+            update_attrs('', '','','select');
+            scope.$apply(function() {
+              ngModel.$setViewValue(undefined);
             });
-            update_attrs(ext,mime,state);
-          };
+          });
 
-          input.on('change', file_selected);
-
-          scope.clear = function() {
-            scope.path = '';
-            nuFile.$put_src(null);
-            update_attrs('','','selected');
-          };
-
-          //update_attrs('pdf','','selected');
+          input.on('change', function(event) {
+            if( event.currentTarget.files.length > 0 ) {
+              scope.$apply(function() {
+                ngModel.$setViewValue(event.currentTarget.files[0]);
+                ngModel.$viewValue = fileFormatter(ngModel.$viewValue);
+                ngModel.$render();
+              });
+            }
+          });
         };
 
         return link;
---
+++ test/protractor/nu.file.chooser.js
@@ -0,0 +1,19 @@
+/*global describe, expect, it, browser, require: true*/ // element
+var pressButtonNode = require('./util.js').pressButtonNode;
+var util = require('./util.js');
+
+var get_unit = util.get_unit;
+
+describe('nu switch', function() {
+  'use strict';
+/*
+  it('should have input checked to false', function() {
+    browser.get('#/nu.file.chooser');
+
+    var unit = get_unit(1);
+    var derivative = new pressButtonNode(unit.find('.nu.file.chooser'));
+    
+    expect(derivative.input.isSelected()).toEqual(false);
+  });
+*/
+});
