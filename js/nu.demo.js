9926a900538483c05885fdba8ddea4fc4bc8d0c2
Kamalakar Gadireddy <kamal590@gmail.com>
2014-03-25 15:57:39 +0530

    Update

--- js/nu.demo.js
+++ js/nu.demo.js
@@ -6,27 +6,27 @@
 	demo.config(function($routeProvider, $locationProvider) {
 	  $routeProvider
     .when('/ngul/nuSwitch', {
-	    templateUrl: 'pages/Switch.html',
+	    templateUrl: '/ngul/pages/Switch.html',
 	    controller: angular.noop
     })
 	  .when('/ngul/nuPressButton', {
-	    templateUrl: 'pages/PB.html',
+	    templateUrl: '/ngul/pages/PB.html',
 	    controller: angular.noop
 	  })
     .when('/ngul/nuList', {
-      templateUrl: 'pages/List.html',
+      templateUrl: '/ngul/pages/List.html',
       controller: angular.noop
     })
     .when('/ngul/nuFileChooser', {
-      templateUrl: 'pages/FC.html',
+      templateUrl: '/ngul/pages/FC.html',
       controller: angular.noop
     })
     .when('/ngul/nuShow', {
-      templateUrl: 'pages/Show.html',
+      templateUrl: '/ngul/pages/Show.html',
       controller: angular.noop
     })
     .when('/ngul', {
-      templateUrl: 'pages/Main.html',
+      templateUrl: '/ngul/pages/Main.html',
       controller: angular.noop
     });
 	 
